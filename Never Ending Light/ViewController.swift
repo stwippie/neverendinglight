3fc8f2aaee8097994ceae3c8b126c97f04d520a1
Yulia Ilina <yilina@evolutiongaming.com>
2021-04-25 20:51:11 +0300

    Initial Commit

---
+++ Never Ending Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Never Ending Light
+//
+//  Created by Yulia Ilina on 25/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
